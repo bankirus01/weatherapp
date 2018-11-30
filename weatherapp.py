d1be8844a53d7de5a6547d2efcd9e3091c653fb2
bankirus01 <bankirus01@gmail.com>
2018-11-30 12:17:41 +0200

    Getting page and temperature from Accuweather

--- weatherapp.py
+++ weatherapp.py
@@ -2,4 +2,29 @@
 
 # This is my first project, it will work !
 
-print('Hi, i starting make i project')
+import html
+from urllib.request import urlopen, Request
+
+# Погода Киев
+ACCU_URL = "https://www.accuweather.com/uk/ua/kyiv/324505/weather-forecast/324505"
+
+headers = {'User-Agent': 'Chrome (X11; Mint; Linux x86_64)'}
+
+accu_request = Request(ACCU_URL, headers=headers)
+accu_page = urlopen(accu_request).read()
+accu_page = accu_page.decode('utf-8')
+
+ACCU_TEMP_TAG = '<span class="large-temp">'
+accu_temp_tag_size = len(ACCU_TEMP_TAG)
+accu_temp_tag_index = accu_page.find(ACCU_TEMP_TAG)
+accu_temp_value_start = accu_temp_tag_index + accu_temp_tag_size
+accu_temp = ''
+
+for char in accu_page[accu_temp_value_start:]:
+    if char != '<':
+        accu_temp += char
+    else:
+        break
+
+print('Accuweather: \n')
+print(f'Temperature: {html.unescape(accu_temp)}\n')
